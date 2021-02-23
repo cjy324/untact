d36dd8124078304e7c70ce622143f4d1dd6bcf72
cjy324 <juy32400@naver.com>
2021-02-23 19:50:33 +0900

    21.02.23 13강 게시물 추가까지 완료

---
+++ src/main/java/com/sbs/untact/controller/UsrArticleController.java
@@ -0,0 +1,54 @@
+package com.sbs.untact.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sbs.untact.dto.Article;
+
+@Controller
+public class UsrArticleController {
+
+	private List<Article> articles;
+	private int articleLastId;
+
+	public UsrArticleController() {
+		// 멤버변수 초기화
+		articleLastId = 0;
+		articles = new ArrayList<>();
+
+		articles.add(new Article(++articleLastId, "2021-02-23 18:18:18", "제목1", "내용1"));
+		articles.add(new Article(++articleLastId, "2021-02-23 18:18:18", "제목2", "내용2"));
+	}
+
+	@RequestMapping("/usr/article/detail")
+	@ResponseBody
+	// 스프링부트: 알아서 json형태로 바꿔 출력값을 리턴해준다.
+	public Article showDetail(int id) {
+		return articles.get(id - 1);
+	}
+
+	@RequestMapping("/usr/article/list")
+	@ResponseBody
+	public List<Article> showList() {
+		return articles;
+	}
+
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Map<String, Object> doAdd(String regDate, String title, String body) {
+		articles.add(new Article(++articleLastId, regDate, title, body));
+
+		Map<String, Object> rs = new HashMap<>();
+		rs.put("resultCode", "S-1");
+		rs.put("msg", "성공");
+		rs.put("id", articleLastId);
+		
+		return rs;
+	}
+}
---
+++ src/main/java/com/sbs/untact/controller/UsrHomeController.java
@@ -0,0 +1,21 @@
+package com.sbs.untact.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+//어노테이션
+//이 클래스가 컨트롤러 클래스라는것을 알려주는 것
+//어떤 클래스를 만들면 스프링에게 이 클래스의 용도를 알려주어야 함
+public class UsrHomeController {
+	
+	// http://localhost:8024/usr/home/main
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	// 만약 "/usr/home/main" 요청이 들어오면 showMain()가 실행되도록 하는 것
+	public String showMain() {
+		System.out.println("hello test");
+		return "Hello!!!!????!!!!!";
+	}
+}
