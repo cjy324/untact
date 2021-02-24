6670f8393e401ed2678e6f865ba2d4b0b857a5cf
cjy324 <juy32400@naver.com>
2021-02-24 12:45:35 +0900

    21.02.24 lombok 적용

--- src/main/java/com/sbs/untact/dto/Article.java
+++ src/main/java/com/sbs/untact/dto/Article.java
@@ -1,5 +1,8 @@
 package com.sbs.untact.dto;
 
+import lombok.Data;
+
+@Data
 public class Article {
 	
 	private int id;
@@ -18,53 +21,4 @@ public class Article {
 		
 	}
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getRegDate() {
-		return regDate;
-	}
-
-	public void setRegDate(String regDate) {
-		this.regDate = regDate;
-	}
-
-	public String getUpdateDate() {
-		return updateDate;
-	}
-
-	public void setUpdateDate(String updateDate) {
-		this.updateDate = updateDate;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getBody() {
-		return body;
-	}
-
-	public void setBody(String body) {
-		this.body = body;
-	}
-
-	@Override
-	public String toString() {
-		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", title=" + title
-				+ ", body=" + body + "]";
-	}
-
-	
-	
-	
 }
