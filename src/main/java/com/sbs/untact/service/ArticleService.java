445f5b846701757597da73d3fb7e44330a26dc25
cjy324 <juy32400@naver.com>
2021-02-25 15:41:03 +0900

    21.02.25 Mybatis 도입

--- src/main/java/com/sbs/untact/service/ArticleService.java
+++ src/main/java/com/sbs/untact/service/ArticleService.java
@@ -1,6 +1,5 @@
 package com.sbs.untact.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Service;
 import com.sbs.untact.dao.ArticleDao;
 import com.sbs.untact.dto.Article;
 import com.sbs.untact.dto.ResultData;
-import com.sbs.untact.util.Util;
 
 //일반 자바에서는 다른 곳에서 ArticleService를 사용하려면
 //ArticleService articleService = new ArticleService();
@@ -34,17 +32,15 @@ public class ArticleService {
 	}
 
 	public ResultData addArticle(String title, String body) {
-		int id = articleDao.addArticle(title, body);
+		articleDao.addArticle(title, body);
 
+		int id = 1; //임시
+		
 		return new ResultData("S-1", "성공하였습니다.", "id", id);
 	}
 
 	public ResultData deleteArticle(int id) {
-		boolean rs = articleDao.deleteArticle(id);
-
-		if (rs == false) {
-			return new ResultData("F-1", "해당 게시물은 존재하지 않습니다.", "id", id);
-		}
+		articleDao.deleteArticle(id);
 
 		return new ResultData("S-1", "삭제하였습니다.", "id", id);
 	}
