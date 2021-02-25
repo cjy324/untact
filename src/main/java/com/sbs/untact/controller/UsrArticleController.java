2b890de2f3c66824ade39fa3513c916782f98e9d
cjy324 <juy32400@naver.com>
2021-02-25 12:38:07 +0900

    21.02.25 ArticleService 도입

--- src/main/java/com/sbs/untact/controller/UsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/UsrArticleController.java
@@ -3,104 +3,72 @@ package com.sbs.untact.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sbs.untact.dto.Article;
 import com.sbs.untact.dto.ResultData;
+import com.sbs.untact.service.ArticleService;
 import com.sbs.untact.util.Util;
 
 @Controller
 public class UsrArticleController {
 
-	private List<Article> articles;
-	private int articleLastId;
-
-	public UsrArticleController() {
-		// 멤버변수 초기화
-		articleLastId = 0;
-		articles = new ArrayList<>();
-
-		//init용 article
-		articles.add(new Article(++articleLastId, Util.getNowDateStr(), Util.getNowDateStr(), "제목1", "내용1"));
-		articles.add(new Article(++articleLastId, Util.getNowDateStr(), Util.getNowDateStr(), "제목2", "내용2"));
-		articles.add(new Article(++articleLastId, Util.getNowDateStr(), Util.getNowDateStr(), "제목3", "내용3"));
-		articles.add(new Article(++articleLastId, Util.getNowDateStr(), Util.getNowDateStr(), "제목4", "내용4"));
-	}
+	@Autowired
+	private ArticleService articleService;
 
 	@RequestMapping("/usr/article/detail")
 	@ResponseBody
 	// 스프링부트: 알아서 json형태로 바꿔 출력값을 리턴해준다.
 	public Article showDetail(int id) {
-		return articles.get(id - 1);
+		return articleService.getArticle(id);
 	}
 
 	@RequestMapping("/usr/article/list")
 	@ResponseBody
 	public List<Article> showList() {
-		return articles;
+		return articleService.getArticles();
 	}
 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public ResultData doAdd(String title, String body) {
-		
-		String regDate = Util.getNowDateStr();
-		String updateDate = Util.getNowDateStr();
-		
-		articles.add(new Article(++articleLastId, regDate, updateDate, title, body));
+	
+		if(title == null) {
+			return new ResultData("F-1", "title을 입력해주세요.");
+		}
+		if(body == null) {
+			return new ResultData("F-1", "body를 입력해주세요.");
+		}
 
-		return new ResultData("S-1", "성공하였습니다.", "id", articleLastId);
+		return articleService.add(title, body);
 	}
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public ResultData doDelete(int id) {
 
-		boolean deleteArticleRs = deleteArticle(id);
+		Article article = articleService.getArticle(id);
 
-		if (deleteArticleRs == false) {
+		if (article == null) {
 			return new ResultData("F-1", "해당 게시물은 존재하지 않습니다.");
 		}
 		
-		return new ResultData("S-1", id + "번 게시물을 삭제하였습니다.");
+		return articleService.deleteArticle(id);
 	}
 
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public ResultData doModify(int id, String title, String body) {
 
-		String updateDate = Util.getNowDateStr();
-		
-		Article selArticle = null;
+		Article article = articleService.getArticle(id);
 
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				selArticle = article;
-				break;
-			}
-		}
-
-		if (selArticle == null) {
+		if (article == null) {
 			return new ResultData("F-1", "해당 게시물은 존재하지 않습니다.", "id", id);
 		}
 
-		selArticle.setUpdateDate(updateDate);
-		selArticle.setTitle(title);
-		selArticle.setBody(body);
-
-		return new ResultData("S-1", "성공", "id", id);
-	}
-
-	private boolean deleteArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				articles.remove(article);
-				return true;
-			}
-		}
-
-		return false;
+		return articleService.modify(id, title, body);
 	}
 }
---
+++ src/main/java/com/sbs/untact/service/ArticleService.java
@@ -0,0 +1,82 @@
+package com.sbs.untact.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sbs.untact.dto.Article;
+import com.sbs.untact.dto.ResultData;
+import com.sbs.untact.util.Util;
+
+//일반 자바에서는 다른 곳에서 ArticleService를 사용하려면
+//ArticleService articleService = new ArticleService();
+//이런식으로 새로운 객체를 만들어 연결해 주었어야하지만
+//스프링부트에서는 @Component를 달아주면  = new ArticleService();를 생략해도 된다.
+//대신 이 객체를 연결하려는 장소에서 @Autowired를 달아주어야 한다.
+//@Autowired는 이정표 같은 개념
+//또한, 이 객체가 service라면 @Service만 달아주고 @Component를 생략한다.
+//@Component
+@Service
+public class ArticleService {
+	private List<Article> articles;
+	private int articleLastId;
+
+	public ArticleService() {
+		// 멤버변수 초기화
+		articleLastId = 0;
+		articles = new ArrayList<>();
+
+		// init용 article
+		articles.add(new Article(++articleLastId, Util.getNowDateStr(), Util.getNowDateStr(), "제목1", "내용1"));
+		articles.add(new Article(++articleLastId, Util.getNowDateStr(), Util.getNowDateStr(), "제목2", "내용2"));
+		articles.add(new Article(++articleLastId, Util.getNowDateStr(), Util.getNowDateStr(), "제목3", "내용3"));
+		articles.add(new Article(++articleLastId, Util.getNowDateStr(), Util.getNowDateStr(), "제목4", "내용4"));
+	}
+
+	public Article getArticle(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+
+		return null;
+	}
+
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+	public ResultData add(String title, String body) {
+		int id = ++articleLastId;
+		String regDate = Util.getNowDateStr();
+		String updateDate = regDate;
+
+		articles.add(new Article(id, regDate, updateDate, title, body));
+
+		return new ResultData("S-1", "성공하였습니다.", "id", id);
+	}
+
+	public ResultData deleteArticle(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				articles.remove(article);
+				return new ResultData("S-1", "삭제하였습니다.", "id", id);
+			}
+		}
+
+		return new ResultData("F-1", "해당 게시물은 존재하지 않습니다.", "id", id);
+	}
+
+	public ResultData modify(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		article.setTitle(title);
+		article.setBody(body);
+		article.setUpdateDate(Util.getNowDateStr());
+
+		return new ResultData("S-1", "게시물을 수정하였습니다.", "id", id);
+	}
+
+}
