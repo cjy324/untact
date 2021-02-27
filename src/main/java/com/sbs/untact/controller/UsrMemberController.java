11874a34dc7a898983611257c8701209d6cc6a10
cjy324 <juy32400@naver.com>
2021-02-27 14:00:14 +0900

    21.02.27 로그인,로그아웃 기능 구현

--- src/main/java/com/sbs/untact/controller/UsrMemberController.java
+++ src/main/java/com/sbs/untact/controller/UsrMemberController.java
@@ -2,6 +2,8 @@ package com.sbs.untact.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +28,9 @@ public class UsrMemberController {
 			return new ResultData("F-1", "loginId를 입력해주세요.");
 		}
 		
-		Member existingMemmember = memberService.getMemberByLoginId((String) param.get("loginId"));
+		Member existingMember = memberService.getMemberByLoginId((String) param.get("loginId"));
 
-		if (existingMemmember != null) {
+		if (existingMember != null) {
 			return new ResultData("F-2", String.format("%s (은)는 이미 사용중인 로그인아이디 입니다.", param.get("loginId")));
 		}
 		
@@ -50,6 +52,53 @@ public class UsrMemberController {
 
 		return memberService.join(param);
 	}
+	
+	@RequestMapping("/usr/member/doLogin")
+	@ResponseBody
+	public ResultData doLogin(String loginId, String loginPw, HttpSession session) {
+		//HttpSession session
+		//servlet에서와는 달리 스프링에선 session을 바로 요청해서 가져올 수 있다.
+		// ex) servlet에서는 requst를 통해 session을 요청하고 다시 HttpSession로 session 값을 가져왔었다.
+		
+		//기 로그인 회원 여부 확인
+		if (session.getAttribute("loginedMemberId") != null) {
+			return new ResultData("F-4", "이미 로그인 상태입니다.");
+		}
 
+		if (loginId == null) {
+			return new ResultData("F-1", "loginId를 입력해주세요.");
+		}
+
+		Member existingMember = memberService.getMemberByLoginId(loginId);
+
+		if (existingMember == null) {
+			return new ResultData("F-2", "존재하지 않는 로그인아이디 입니다.", "loginId", loginId);
+		}
+
+		if (loginPw == null) {
+			return new ResultData("F-1", "loginPw를 입력해주세요.");
+		}
+
+		if (existingMember.getLoginPw().equals(loginPw) == false) {
+			return new ResultData("F-3", "비밀번호가 일치하지 않습니다.");
+		}
+
+		//세션에 로그인 회원 id 등록
+		session.setAttribute("loginedMemberId", existingMember.getId());
+
+		return new ResultData("S-1", String.format("%s님 환영합니다.", existingMember.getNickname()));
+	}
+
+	@RequestMapping("/usr/member/doLogout")
+	@ResponseBody
+	public ResultData doLogout(HttpSession session) {
+		if (session.getAttribute("loginedMemberId") == null) {
+			return new ResultData("S-2", "이미 로그아웃 상태입니다.");
+		}
+
+		session.removeAttribute("loginedMemberId");
+
+		return new ResultData("S-1", "로그아웃 되었습니다.");
+	}
 	
 }
