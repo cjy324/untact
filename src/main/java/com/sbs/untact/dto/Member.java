3f5977ab4345811c3a33ca7c28b1ff0cba1f17b5
cjy324 <juy32400@naver.com>
2021-03-01 19:24:29 +0900

    21.03.01 authKey로 회원정보 확인기능 구현

--- src/main/java/com/sbs/untact/dto/Member.java
+++ src/main/java/com/sbs/untact/dto/Member.java
@@ -1,6 +1,8 @@
 package com.sbs.untact.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 
 
@@ -10,7 +12,10 @@ public class Member {
 	private String regDate;
 	private String updateDate;
 	private String loginId;
+	@JsonIgnore
+	//@JsonIgnore: json으로 보이지 않게 할 수 있다.
 	private String loginPw;
+	@JsonIgnore
 	private String authKey;
 	private String name;
 	private String nickname;
