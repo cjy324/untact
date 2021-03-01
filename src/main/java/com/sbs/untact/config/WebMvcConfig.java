eab6e11855cf35b47e35ec4b3e0a4392a631ffd1
cjy324 <juy32400@naver.com>
2021-03-01 13:56:15 +0900

    21.03.01 댓글 테이블 구조 변경 및 리스팅 기능 구현

--- src/main/java/com/sbs/untact/config/WebMvcConfig.java
+++ src/main/java/com/sbs/untact/config/WebMvcConfig.java
@@ -37,6 +37,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 				.excludePathPatterns("/usr/member/login").excludePathPatterns("/usr/member/doLogin")
 				.excludePathPatterns("/usr/member/join").excludePathPatterns("/usr/member/doJoin")
 				.excludePathPatterns("/usr/article/list").excludePathPatterns("/usr/article/detail")
+				.excludePathPatterns("/usr/reply/list")
 				.excludePathPatterns("/usr/member/findLoginId").excludePathPatterns("/usr/member/doFindLoginId")
 				.excludePathPatterns("/usr/member/findLoginPw").excludePathPatterns("/usr/member/doFindLoginPw")
 				.excludePathPatterns("/usr/file/test*").excludePathPatterns("/usr/file/doTest*")
