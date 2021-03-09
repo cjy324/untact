cb4dfca01f39b5c231d5e6de984a54ef0c89ad8a
cjy324 <juy32400@gmil.com>
2021-03-09 10:55:09 +0900

    21.03.09 관리자 회원 리스트 추가

--- src/main/java/com/sbs/untact/dao/MemberDao.java
+++ src/main/java/com/sbs/untact/dao/MemberDao.java
@@ -1,5 +1,6 @@
 package com.sbs.untact.dao;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -9,9 +10,19 @@ import com.sbs.untact.dto.Member;
 
 @Mapper
 public interface MemberDao {
+
+	List<Member> getForPrintMembers(@Param("searchKeywordType") String searchKeywordType,
+			@Param("searchKeyword") String searchKeyword, @Param("limitStart") int limitStart,
+			@Param("limitTake") int limitTake);
+
 	void join(Map<String, Object> param);
+
 	Member getMember(@Param("id") int id);
+
 	Member getMemberByLoginId(@Param("loginId") String loginId);
+
 	void modifyMember(Map<String, Object> param);
+
 	Member getMemberByAuthKey(@Param("authKey") String authKey);
+
 }
